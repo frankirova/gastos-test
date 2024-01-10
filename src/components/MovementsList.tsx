aadcd930c28ba8a7c6519536f32059cbaf039d94
frankirova <franciscor1905@gmail.com>
2024-01-10 14:53:18 -0300

    change width movements

--- src/components/MovementsList.tsx
+++ src/components/MovementsList.tsx
@@ -5,11 +5,11 @@ import { ButtonAdd } from "./ButtonAdd"
 import { Link } from "react-router-dom"
 import { useMovements } from "../store/movements.store"
 import { Movement } from "../store/types/movements.type"
+
 export const MovementsList = () => {
     const [selectGroup, setSelectGroup] = useState('')
     const { getMovements, movements }: any = useMovements()
     const filtered_movements = movements.filter((movement: any) => movement.group === selectGroup)
-
 
     useEffect(() => {
         getMovements()
@@ -29,13 +29,13 @@ export const MovementsList = () => {
                             to={`/${movement._id}`}
                             backgroundColor={'#2b2b2b'}
                             padding={6}
-                            minWidth='80vw'
-                            minHeight={'4rem'}
+                            minWidth='90vw'
+                            maxHeight={'6rem'}
                             color={'white'}
                             justifyContent={'space-evenly'}
                             alignItems={'center'}
                         >
-                            <Image minW={'5rem'} src="https://placehold.co/48x48" borderRadius={'100%'} />
+                            <Image minW={'5rem'} src="https://placehold.co/24x24" borderRadius={'100%'} />
                             <Text maxW={'5rem'}>{movement.category}</Text>
                             <Tag minW={'5rem'}>{new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS' }).format(movement.amount)}</Tag>
                         </HStack>
